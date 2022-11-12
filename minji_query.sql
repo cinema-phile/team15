235db0492464c12caaa2835d628f46e5568c6e64
Minji Kim <79117648+minji1289@users.noreply.github.com>
2022-11-13 07:49:51 +0900

    cast_nm NOT NULL

--- minji_query.sql
+++ minji_query.sql
@@ -2,21 +2,21 @@
 
 -- *'여성' 버튼 선택*
 -- '여성' 배역에 대한 랭킹 결과 조회
-select cast_nm from characters where sex='여자' and character_id IN (select character_id from character_ranking order by vote desc);
+select cast_nm from characters where cast_nm!='' and sex='여자' and character_id IN (select character_id from character_ranking order by vote desc);
 -- '여성' 배역에 대한 랭킹 TOP3 결과 조회
-select cast_nm from characters where sex='여자' and character_id IN (select character_id from character_ranking order by vote desc) limit 3;
+select cast_nm from characters where cast_nm!='' and sex='여자' and character_id IN (select character_id from character_ranking order by vote desc) limit 3;
 
 -- *'남성' 버튼 선택*
 -- '남성' 배역에 대한 랭킹 결과 조회
-select cast_nm from characters where sex='남자' and character_id IN (select character_id from character_ranking order by vote desc);
+select cast_nm from characters where cast_nm!='' and sex='남자' and character_id IN (select character_id from character_ranking order by vote desc);
 -- '남성' 배역에 대한 랭킹 TOP3 결과 조회
-select cast_nm from characters where sex='남자' and character_id IN (select character_id from character_ranking order by vote desc)  limit 3;
+select cast_nm from characters where cast_nm!='' and sex='남자' and character_id IN (select character_id from character_ranking order by vote desc)  limit 3;
 
 -- *'전체보기'*
 -- 배역에 대한 랭킹 결과 조회 (조건 없음)
-select cast_nm from characters where character_id IN (select character_id from character_ranking order by vote desc);
+select cast_nm from characters where cast_nm!='' and character_id IN (select character_id from character_ranking order by vote desc);
 -- 배역에 대한 랭킹 결과 TOP3
-select cast_nm from characters where character_id IN (select character_id from character_ranking order by vote desc) limit 3;
+select cast_nm from characters where cast_nm!='' and character_id IN (select character_id from character_ranking order by vote desc) limit 3;
 
 
 -- ** VOTE 페이지 ** --
