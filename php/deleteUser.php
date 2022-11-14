380ce460539a4ea39f20d850d508ad08915fdda8
Minji Kim <79117648+minji1289@users.noreply.github.com>
2022-11-14 09:25:35 +0900

    Update deleteUser.php

--- php/deleteUser.php
+++ php/deleteUser.php
@@ -1,7 +1,11 @@
 <?php
 
+if(!isset($_SESSION['userId'])) {
+	/*login으로 redirect하는 부분 추가하기*/
+    echo '세션 만료';
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = mysqli_connect("localhost", "root", "", "potatoo");
+$mysqli = mysqli_connect("localhost", "team15", "team15", "team15");
 
 if (mysqli_connect_errno()) {
 	printf("Connection failed: %s\n", mysqli_connect_error());
@@ -10,22 +14,23 @@ if (mysqli_connect_errno()) {
 
 $mysqli->begin_transaction();
 
-/* userid 변수 받아오는 부분 추가해야함! */
 
 try{
-	$mysqli->query("delete from comment_free where userid='$userid'");
-	$mysqli->query("delete from comment_information where userid='$userid'");
-	$mysqli->query("delete from board_free where userid='$userid'");
-	$mysqli->query("delete from board_information where userid='$userid'");
-	$mysqli->query("delete from test_result where userid='$userid'");
-	$mysqli->query("delete from star_movie where userid='$userid'");
-	$mysqli->query("delete from star_people where userid='$userid'");
-	$mysqli->query("delete from watch_movie where userid='$userid'");
-	$mysqli->query("delete from users where userid='$userid'");
+	$mysqli->query("delete from comment_free where userid ='".$_SESSION['userId']."'");
+	$mysqli->query("delete from comment_information where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from board_free where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from board_information where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from test_result where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from star_movie where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from star_people where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from watch_movie where userid='".$_SESSION['userId']."'");
+	$mysqli->query("delete from users where userid='".$_SESSION['userId']."'");
 
 	/* If code reaches this point without errors, then commit the data */
 	$mysqli->commit();
 	echo 'commit';
+	unset($_SESSION['userId']);
+	echo 'unset session';
 
 } catch (mysqli_sql_exception on $exception) {
 	$mysqli->rollback();
