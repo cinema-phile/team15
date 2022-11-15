5b786e251dbcd07152ea9e5735283e06562edba7
seohuizzang <kkseohui@gmail.com>
2022-11-16 06:31:08 +0900

    movie -> star

--- pages/search/filmInfo.php
+++ pages/search/filmInfo.php
@@ -4,12 +4,9 @@ header('Content-Type: text/html; charset=utf-8');
 if (!session_id()) {
     session_start();
 }
-
-$watch_rate = 0;
-$code = $_GET['code']; /*movie code from url*/
-$movie_info = array();
-$watched = false;
-$txt = "안 봤어요!";
+$filmoList = array();
+$code = $_GET['code'];
+$isStar = false;
 
 # DB Connection
 $conn = mysqli_connect("localhost", "team15", "team15", "team15");
@@ -19,10 +16,81 @@ if (mysqli_connect_errno()) {
     echo "<script>alert('connect error');</script>";
     exit();
 }
-
 else {
+    $sql = "select people_nm, people_nm_en, sex, profile from people where people_cd = ?;";
 
-    $sql1 = "select EXISTS (select * from watch_movie where userid = ? and movie_cd = ?);";
+    if($stmt = mysqli_prepare($conn, $sql)) {
+        if (mysqli_stmt_bind_param($stmt, "s", $code)) {
+            if (mysqli_stmt_execute($stmt)) {
+                if ($res = mysqli_stmt_get_result($stmt)) {
+                    while ($newArray = mysqli_fetch_array($res)) {
+
+                        $people_nm = $newArray['people_nm'];
+                        $people_nm_en = $newArray['people_nm_en'];
+
+                        if ($newArray['profile'] != NULL) {
+                            $profile = " src= '".$newArray['profile']."'";
+                        }
+                        else if ($newArray['profile'] == NULL && $newArray['sex'] == "여자") {
+                            $profile = " src= '../../img/woman.png'";
+                        }
+                        else {
+                            $profile = " src= '../../img/man.png'";
+                        }
+
+                    }}}}}
+
+    $sql3 = "insert into star_people (userid, people_cd) values (?, ?);";
+
+    $sql2 = "select m.movie_cd, m.movie_nm, m.imgUrl, c.cast_nm
+            from characters as c
+            left join movie as m on m.movie_cd = c.movie_cd
+            left join people as p on c.people_cd = p.people_cd
+            where c.people_cd = ?;";
+
+    if($stmt = mysqli_prepare($conn, $sql2)) {
+        if (mysqli_stmt_bind_param($stmt, "s", $code)) {
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_bind_result($stmt, $movie_cd, $movie_nm, $imgUrl, $cast_nm);
+
+                while(mysqli_stmt_fetch($stmt)) {
+                    array_push($filmoList, [
+                        "movie_cd" => $movie_cd,
+                        "movie_nm" => $movie_nm,
+                        "imgUrl" => $imgUrl,
+                        "cast_nm" => $cast_nm
+                    ]);
+                }
+            }
+        }
+    }
+
+    # USER 팬지수 계산
+    $cnt = 0;
+    $sql = "select EXISTS (
+                select * from watch_movie where userid = ? and movie_cd = ?
+            )";
+    if($stmt = mysqli_prepare($conn, $sql)) {
+        for ($i=0; $i < count($filmoList); $i++) { 
+            $movie_cd = $filmoList[$i]['movie_cd'];
+            if(mysqli_stmt_bind_param($stmt, 'ss', $userId, $movie_cd)) {
+                if(mysqli_stmt_execute($stmt)) {
+                    mysqli_stmt_bind_result($stmt, $watched);
+                    while(mysqli_stmt_fetch($stmt)) {
+                        if($watched == 1) {
+                            $cnt = $cnt + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    $fanRate = round($cnt / count($filmoList) * 100, 2);
+
+    # 즐겨찾기 등록 / 해제
+    $sql1 = "select EXISTS (select * from star_people where userid = ? and people_cd = ?);";
 
     if($stmt = mysqli_prepare($conn, $sql1)) {
         if (mysqli_stmt_bind_param($stmt, 'ss', $userId, $code)) {
@@ -31,60 +99,19 @@ else {
 
                 while(mysqli_stmt_fetch($stmt)) {
                     if ($res == 1) {
-                        $watched = true;
-                        $txt = "봤어요!";
+                        $isStar = true;
                     }
                     else {
-                        $watched = false;
-                        $txt = "안 봤어요!";
+                        $isStar = false;
     }}}}}
 
-    $sql0 = "select cntMovie, cntUser from ( 
-                select movie_cd, count(movie_cd) as 'cntMovie'
-                from watch_movie
-                group by movie_cd
-            ) c, (
-                select count(*) as 'cntUser'
-                from users
-            ) u where c.movie_cd = ?;";
+    # close connection
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 
-    if($stmt = mysqli_prepare($conn, $sql0)) {
-        if (mysqli_stmt_bind_param($stmt, "s", $code)) {
-            if (mysqli_stmt_execute($stmt)) {
-                if ($res = mysqli_stmt_get_result($stmt)) {
-                    while ($newArray = mysqli_fetch_array($res)) {
-                        $num = $newArray['cntMovie'] / $newArray['cntUser'] * 100;
-                        $watch_rate = round($num, 2);
-    }}}}};
-
-    $sql = "select m.imgUrl, m.movie_nm, m.directors, m.open_yr, m.genre, m.story,
-            c.cast_nm, p.people_cd, p.people_nm, p.profile, p.sex
-            from movie as m
-            left join characters as c on m.movie_cd = c.movie_cd
-            left join people as p on c.people_cd = p.people_cd
-            where m.movie_cd = ?";
-
-    if($stmt = mysqli_prepare($conn, $sql)) {
-        if (mysqli_stmt_bind_param($stmt, "s", $code)) {
-            if (mysqli_stmt_execute($stmt)) {
-                mysqli_stmt_bind_result($stmt, $imgUrl, $movie_nm, $directors, $open_yr, $genre, $story, 
-                                        $cast_nm, $people_cd, $people_nm, $profile, $sex);
-                    while (mysqli_stmt_fetch($stmt)) {
-                        array_push($movie_info, [
-                            "imgUrl" => $imgUrl,
-                            "movie_nm" => $movie_nm,
-                            "directors" => $directors,
-                            "open_yr" => $open_yr,
-                            "genre" => $genre,
-                            "story" => $story,
-                            "cast_nm" => $cast_nm,
-                            "people_cd" => $people_cd,
-                            "people_nm" => $people_nm,
-                            "profile" => $profile,
-                            "sex" => $sex
-                        ]); 
-    }}}}}
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -93,13 +120,13 @@ else {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../index.css">
-    <link rel="stylesheet" href="FILMstyle.css">
-    <title>FILM</title>
+    <link rel="stylesheet" href="filmoStyle.css">
+    <title>FILMOGRAPHY</title>
 </head>
 <body>
     <div id="contents">
         <header class="title">
-            <h1>FILM</h1>
+            <h1>FILMOGRAPHY</h1>
         </header>
         <section class="menu">
             <a href="../search/search.php"><h4 class="eachMenu">SEARCH</h4></a>
@@ -108,81 +135,56 @@ else {
             <a href="../community/index.php"><h4 class="eachMenu">COMMUNITY</h4></a>
             <a href="../mypage/index.php"><h4 class="eachMenu">MYPAGE</h4></a>
         </section>
-        
-        <section class="filmInfo">
-            <div class="topLayout">
-                <div class="poster">
-                    <img src="<?=$movie_info[0]['imgUrl']?>" width="240px" height="313px">
-                </div>
-                <div class="infoLayout">
-                    <div class="titleNtag">
-                        <p class="filmTitle"><?=$movie_info[0]['movie_nm']?></p>
-                        <?php
-                        if($watched) {
-                        ?>
-                        <img id="tag" src="../../img/tag.svg">
-                        <?php
-                        } else {
-                        ?>
-                        <div id="tag"></div>
-                        <?php
-                        }
-                        ?>
-                    </div>
-                    <div class="detailinfo">
-                        <p>감독 | <?=$movie_info[0]['directors']?></p>
-                        <p>개봉연도 | <?=$movie_info[0]['open_yr']?></p>
-                        <p>장르 | <?=$movie_info[0]['genre']?></p>
-                    </div>
-                    <div class="watched">회원의 <span id="span"><?=$watch_rate?>%</span>가 이 영화를 관람했습니다</div>
-                    <form action="../../php/search/watched_insert.php?movie_cd=<?=$code?>" method="post">
-                    <button class="watchedBtn" type="submit"><?=$txt?></button>
-                    </form>
-                </div> 
-            </div>
-            <p class="listTitle">스토리</p>
-            <h5 class="story"><?=$movie_info[0]['story']?></h5>
-            <section class="actorList">
-                <p class="listTitle">출연 배우</p>
-                <div class="actorProfile">
-                    <?php
-                    for ($i=0; $i < count($movie_info); $i++) { 
-                        $url = './filmography.php?code='.$movie_info[$i]["people_cd"];
-                    ?>
-                    <a href="<?=$url?>">
-                        <div class="individualActor">
-                            <div class="actorImg">
 
-                                <?php
-                                if($movie_info[$i]['profile'] != null) {
-                                ?>
-                                <img id="pic" src="<?=$movie_info[$i]['profile']?>"> <!-- profile != null -->
-                                <?php
-                                } else if ($movie_info[$i]['profile'] != null && $movie_info[$i]['sex'] == '여자') {
-                                ?>
-                                <img id="pic" src="../../img/woman.png"> <!-- profile == null && 여자 -->
-                                <?php
-                                } else {
-                                ?>
-                                <img id="pic" src="../../img/man.png"> <!-- else -->
-                                <?php
-                                }
-                                ?>
-                    
-                            </div>
-                            <div class="actorText">
-                                <p class="actorName"><?=$movie_info[$i]['people_nm']?></p>
-                                <p class="actorChar"><?=$movie_info[$i]['cast_nm']?></p>
-                            </div>
-                        </div>
-                    </a>
-                    <?php
-                    }
-                    ?>
-                </div>
-            </section>
+        <section class="profile">
+            <img class="profileimg" <?=$profile?>>
+            <div class="profiletitle">
+            <?php
+            $url = "../../php/search/star_insert.php?people_cd=".$code;
+            if(!$isStar) {
+            ?>
+                <img id="star" src="../../img/star_empty.svg" onclick="location.href='<?=$url?>'">
+            <?php
+            } else {
+            ?>
+                <img id="star" src="../../img/star_full.svg" onclick="location.href='<?=$url?>'">
+            <?php
+            }
+            ?>
+            <div class="nameLayout"><p class="name"><?=$people_nm?></p><br><p class="name_eng"><?=$people_nm_en?></p></div>
+            </div>
         </section>
 
+        <section class="filmolist">
+            <div class="fanscore">
+                <div class="fanscore-L">나의 팬 지수 <span id="fanscorespan"><?=$fanRate?>%</span></div>
+                <p class="fanscore-R"><span id="span"><?=count($filmoList)?></span> 작품 중 <span id="span"><?=$cnt?></span> 개 관람</p>
+            </div>
+            <progress class="fanGraph" min="0" max="100" value="<?=$fanRate?>"></progress>
+
+
+            <div class="characterList">
+                <?php
+                for ($i=0; $i < count($filmoList); $i++) { 
+                    $url = './filmInfo.php?code='.$filmoList[$i]['movie_cd'];
+                ?>
+                <a href="<?=$url?>">
+                <div class="individualChar">
+                    <div class="charProfileImg">
+                        <!-- <img class="tag" src="../../img/tag.svg"> -->
+                        <img id="charPic" src="<?=$filmoList[$i]['imgUrl']?>">
+                    </div>
+                    <div class="charText">
+                        <p class="charName"><?=$filmoList[$i]['cast_nm']?></p>
+                        <p class="charMovie"><?=$filmoList[$i]['movie_nm']?></p>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+                </a>
+            </div>
+        </section>
     </div>
 </body>
 </html>
