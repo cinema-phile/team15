2d61123804f79b40c746336a0ff166d76d45737e
OSSU <90602694+SuHyeon00@users.noreply.github.com>
2022-11-15 16:02:25 +0900

    Edit menu path

--- pages/community/community_newPost.php
+++ pages/community/community_newPost.php
@@ -27,7 +27,7 @@ $isNew=true;
             <a href="../vote/index.html"><h4 class="eachMenu">VOTE</h4></a>
             <a href="../recommend/index.html"><h4 class="eachMenu">RECOMMEND</h4></a>
             <a href="../community/index.php"><h4 class="eachMenu">COMMUNITY</h4></a>
-            <a href="../mypage/index.html"><h4 class="eachMenu">MYPAGE</h4></a>
+            <a href="../mypage/index.php"><h4 class="eachMenu">MYPAGE</h4></a>
         </section>
         <section class="board">
             <div class="post">
